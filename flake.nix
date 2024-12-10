11955fb67a1b9240daa35d894205e312e18ce6b7
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-12-10 22:05:52 +0000

    added flake inputs and shell file

--- flake.nix
+++ flake.nix
@@ -1,1 +1,8 @@
+{
+  description = "shells, mmm...";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs>ref=nixos-unstable";
 
+  outputs = {self, nixpkgs }:
+  let pkgs - nixpkgs.legacyPackages."x86_64-Linux";
+  in {}
+}
